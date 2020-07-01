c2a9b59a4ebccda00224b228efb2f73b9a1df038
oncleben31 <oncleben31@users.noreply.github.com>
2020-07-01 22:52:17 +0000

    Make test_user works

--- tests/components/meteo_france/test_config_flow.py
+++ tests/components/meteo_france/test_config_flow.py
@@ -1,5 +1,6 @@
 """Tests for the Meteo-France config flow."""
 from meteofrance.exceptions import MeteoFranceException
+from meteofrance.model import Place
 import pytest
 
 from homeassistant import data_entry_flow
@@ -14,6 +15,10 @@ CITY_1_POSTAL = "74220"
 CITY_1_NAME = "La Clusaz"
 CITY_1_LAT = 45.90417
 CITY_1_LON = 6.42306
+CITY_1_COUNTRY = "FR"
+CITY_1_ADMIN = "Rhône-Alpes"
+CITY_1_ADMIN2 = "74"
+
 CITY_2_POSTAL_DISTRICT_1 = "69001"
 CITY_2_POSTAL_DISTRICT_4 = "69004"
 CITY_2_NAME = "Lyon"
@@ -26,7 +31,17 @@ def mock_controller_client_1():
         "homeassistant.components.meteo_france.config_flow.MeteoFranceClient",
         update=False,
     ) as service_mock:
-        service_mock.return_value.get_data.return_value = {"name": CITY_1_NAME}
+        city_1 = Place(
+            {
+                "name": CITY_1_NAME,
+                "lat": CITY_1_LAT,
+                "lon": CITY_1_LON,
+                "country": CITY_1_COUNTRY,
+                "admin": CITY_1_ADMIN,
+                "admin2": CITY_1_ADMIN2,
+            }
+        )
+        service_mock.return_value.search_places.return_value = [city_1]
         yield service_mock
 
 
@@ -60,21 +75,18 @@ async def test_user(hass, client_1):
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["step_id"] == "user"
 
-    # test with all provided
+    # test with all provided with search returning only 1 place
     result = await hass.config_entries.flow.async_init(
-        DOMAIN,
-        context={"source": SOURCE_USER},
-        data={
-            CONF_CITY: CITY_1_POSTAL,
-            CONF_LATITUDE: CITY_1_LAT,
-            CONF_LONGITUDE: CITY_1_LON,
-        },
+        DOMAIN, context={"source": SOURCE_USER}, data={CONF_CITY: CITY_1_POSTAL},
     )
     assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
     assert result["result"].unique_id == f"{CITY_1_LAT}, {CITY_1_LON}"
-    assert result["title"] == CITY_1_POSTAL
-    assert result["data"][CONF_LATITUDE] == CITY_1_LAT
-    assert result["data"][CONF_LONGITUDE] == CITY_1_LON
+    assert (
+        result["title"]
+        == f"{CITY_1_NAME} - {CITY_1_ADMIN} ({CITY_1_ADMIN2}) - {CITY_1_COUNTRY}"
+    )
+    assert result["data"][CONF_LATITUDE] == str(CITY_1_LAT)
+    assert result["data"][CONF_LONGITUDE] == str(CITY_1_LON)
 
 
 async def test_import(hass, client_1):
