047248502de1991bc17e0e6420841a1909e4d8b6
Quentin POLLET <polletquentin74@me.com>
2020-07-03 13:05:58 +0200

    Add LinkyAuthorizeCallbackView + log state

--- homeassistant/components/linky/config_flow.py
+++ homeassistant/components/linky/config_flow.py
@@ -3,14 +3,18 @@ import logging
 from typing import Optional
 from urllib.parse import urlencode
 
+from aiohttp import web
+
 from homeassistant import config_entries
+from homeassistant.components.http import HomeAssistantView
 from homeassistant.helpers import config_entry_oauth2_flow
-from homeassistant.helpers.config_entry_oauth2_flow import AUTH_CALLBACK_PATH
 from homeassistant.helpers.network import get_url
 
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
+
+AUTH_CALLBACK_PATH = "/auth/external/linky"
 
 
 class LinkyFlowHandler(
@@ -33,8 +37,11 @@ class LinkyFlowHandler(
     async def async_step_auth(self, user_input: Optional[dict] = None) -> dict:
         """Create an entry for auth."""
         # Flow has been triggered by external data
+        _LOGGER.error("starting_auth_config")
         if user_input:
+            _LOGGER.error("getting_params_from_auth")
             self.external_data = user_input
+            _LOGGER.error(self.external_data)
             return self.async_external_step_done(next_step_id="creation")
 
         params = urlencode(
@@ -45,7 +52,39 @@ class LinkyFlowHandler(
             }
         )
 
+        _LOGGER.error("openning_auth_url")
         return self.async_external_step(
             step_id="auth",
             url=f"http://www.sud-domotique-expert.fr/enedis/accord_enedis_prod.html?{params}",
+        )
+
+
+class LinkyAuthorizeCallbackView(HomeAssistantView):
+    """Linky Authorization Callback View."""
+
+    requires_auth = False
+    url = AUTH_CALLBACK_PATH
+    name = "auth:external:linky"
+
+    async def get(self, request: web.Request) -> web.Response:
+        """Receive authorization code."""
+        if (
+            "refresh_token" not in request.query
+            or "access_token" not in request.query
+            or "usage_point_id" not in request.query
+            or "flow_id" not in request.query
+        ):
+            return web.Response(
+                text=f"Missing refresh_token or access_token or usage_point_id or flow_id parameter in {request.url}"
+            )
+
+        hass = request.app["hass"]
+
+        await hass.config_entries.flow.async_configure(
+            flow_id=request.query["flow_id"], user_input=request.query
+        )
+
+        return web.Response(
+            headers={"content-type": "text/html"},
+            text="<script>window.close()</script>",
         )
