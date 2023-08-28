c4fdf3db74d8af17c762aaf55edfc3566943e925
Quentame <polletquentin74@me.com>
2023-08-28 14:43:22 +0000

    Review: use class as a return value in coordinator data

--- homeassistant/components/epic_games_store/coordinator.py
+++ homeassistant/components/epic_games_store/coordinator.py
@@ -1,9 +1,9 @@
 """The EGS integration."""
 from __future__ import annotations
 
-from datetime import timedelta
+from datetime import datetime, timedelta
 import logging
-from typing import Any
+from typing import TypedDict
 
 from epicstore_api import EpicGamesStoreAPI
 
@@ -18,7 +18,33 @@ SCAN_INTERVAL = timedelta(days=1)
 _LOGGER = logging.getLogger(__name__)
 
 
-class EGSUpdateCoordinator(DataUpdateCoordinator[dict[str, Any]]):
+class GameData(TypedDict):
+    """Game fields."""
+
+    title: str
+    publisher: str
+    url: str
+    img_portrait: str | None
+    img_landscape: str | None
+    original_price: str
+
+
+class FreeGameData(TypedDict):
+    """Free game fields."""
+
+    start_at: datetime
+    end_at: datetime
+    games: list[GameData]
+
+
+class EGSUpdateCoordinatorData(TypedDict):
+    """Formatted API response."""
+
+    free_games: FreeGameData
+    next_free_games: FreeGameData
+
+
+class EGSUpdateCoordinator(DataUpdateCoordinator[EGSUpdateCoordinatorData]):
     """Class to manage fetching data from the Epic Game Store."""
 
     def __init__(
@@ -35,14 +61,14 @@ class EGSUpdateCoordinator(DataUpdateCoordinator[dict[str, Any]]):
             update_interval=SCAN_INTERVAL,
         )
 
-    async def _async_update_data(self) -> dict[str, dict[str, Any]]:
+    async def _async_update_data(self) -> EGSUpdateCoordinatorData:
         """Update data via library."""
         try:
-            data = await self.hass.async_add_executor_job(self._api.get_free_games)
+            raw_data = await self.hass.async_add_executor_job(self._api.get_free_games)
         except Exception as error:
             raise UpdateFailed(error) from error
 
-        _LOGGER.debug(data)
+        _LOGGER.debug(raw_data)
 
         promo_games = sorted(
             filter(
@@ -54,12 +80,12 @@ class EGSUpdateCoordinator(DataUpdateCoordinator[dict[str, Any]]):
                     )
                     or (g["promotions"]["upcomingPromotionalOffers"])
                 ),
-                data["data"]["Catalog"]["searchStore"]["elements"],
+                raw_data["data"]["Catalog"]["searchStore"]["elements"],
             ),
             key=lambda g: g["title"],
         )
 
-        return_data: dict[str, dict[str, Any]] = self.data or {}
+        return_data: EGSUpdateCoordinatorData = self.data or {}
         for game in promo_games:
             game_title = game["title"]
             game_publisher = game["seller"]["name"]
@@ -77,32 +103,42 @@ class EGSUpdateCoordinator(DataUpdateCoordinator[dict[str, Any]]):
             game_promotions = game["promotions"]["promotionalOffers"]
             upcoming_promotions = game["promotions"]["upcomingPromotionalOffers"]
 
-            prefix = ""
+            is_next = False
             promotion_data = {}
             if game_promotions and game["price"]["totalPrice"]["discountPrice"] == 0:
                 promotion_data = game_promotions[0]["promotionalOffers"][0]
             elif not game_promotions and upcoming_promotions:
-                prefix = "next_"
+                is_next = True
                 promotion_data = upcoming_promotions[0]["promotionalOffers"][0]
 
             if promotion_data:
-                return_data[f"{prefix}free_games"] = return_data.get(
-                    f"{prefix}free_games",
-                    {
-                        "start_at": dt_util.parse_datetime(promotion_data["startDate"]),
-                        "end_at": dt_util.parse_datetime(promotion_data["endDate"]),
-                        "games": [],
-                    },
+                free_games: list[GameData] = []
+                free_games += return_data.get(
+                    "next_free_games" if is_next else "free_games", []
                 )
-                return_data[f"{prefix}free_games"]["games"].append(
-                    {
-                        "title": game_title,
-                        "publisher": game_publisher,
-                        "url": game_url,
-                        "img_portrait": game_img_portrait,
-                        "img_landscape": game_img_landscape,
-                        "original_price": game_price.replace("\xa0", " "),
-                    }
+                free_game = FreeGameData(
+                    start_at=dt_util.parse_datetime(promotion_data["startDate"]),
+                    end_at=dt_util.parse_datetime(promotion_data["endDate"]),
+                    games=[
+                        **return_data.get(
+                            "next_free_games" if is_next else "free_games", {}
+                        ).get("games", [])
+                    ],
                 )
+                free_game["games"].append(
+                    GameData(
+                        title=game_title,
+                        publisher=game_publisher,
+                        url=game_url,
+                        img_portrait=game_img_portrait,
+                        img_landscape=game_img_landscape,
+                        original_price=game_price.replace("\xa0", " "),
+                    )
+                )
+                if is_next:
+                    return_data["next_free_games"] = free_game
+                else:
+                    return_data["free_games"] = free_game
 
+        _LOGGER.debug(return_data)
         return return_data
